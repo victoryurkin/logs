8885fd9583a63b80e5fdb41bd0f9e66bd2c38861
Victor Yurkin <victor.yurkin@naxgrp.com>
2024-03-26 22:51:40 -0400

    Init

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -2,8 +2,71 @@
 
 // Print all entries, across all of the *async* sources, in chronological order.
 
-module.exports = (logSources, printer) => {
+const MinHeap = require("../lib/min-heap");
+
+// Simple, straight forward, but inefficient solution
+// Used to test the efficiency of the MinHeap solution
+const inefficientSolution = async (logSources, printer) => {
+  let mergedLogs = [];
+  for (let i = 0; i < logSources.length; i++) {
+    let logEntry = null;
+    while((logEntry = await logSources[i].popAsync())) {
+      mergedLogs.push(logEntry);
+    }
+  }
+
+  const sortedLogs = mergedLogs.sort((a, b) => a.date - b.date);
+
+  for (let i = 0; i < sortedLogs.length; i++) {
+    printer.print(sortedLogs[i]);
+  }
+
+  printer.done();
+}
+
+// Solutions using the MinHeap
+
+const getAllLogsFromSource = async (logSource) => {
+  let logs = [];
+  let logEntry = null;
+  while((logEntry = await logSource.popAsync())) {
+    logs.push(logEntry);
+  }
+  return logs;
+}
+
+const efficientSolution = async (logSources, printer) => {
+  const minHeap = new MinHeap();
+  const promises = [];
+
+  // Insert all elements into the min-heap
+  for (let i = 0; i < logSources.length; i++) {
+    promises.push(getAllLogsFromSource(logSources[i]));
+  }
+
+  Promise.all(promises).then((logs) => {
+    logs.forEach((log) => {
+      log.forEach((entry) => {
+        minHeap.insert(entry);
+      });
+    });
+
+    // Extract elements from the heap to get them in sorted order
+    while (!minHeap.isEmpty()) {
+      printer.print(minHeap.extractMin());
+    }  
+    printer.done();
+  });  
+}
+
+module.exports = (logSources, printer) => { 
   return new Promise((resolve, reject) => {
-    resolve(console.log("Async sort complete."));
+      // inefficientSolution(logSources, printer).then(() => {
+      //   resolve(console.log("Async sort complete."));
+      // });
+
+      efficientSolution(logSources, printer).then(() => {
+        resolve(console.log("Async sort complete."));
+      });
   });
 };
--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,7 +1,46 @@
-"use strict";
+const MinHeap = require("../lib/min-heap");
 
-// Print all entries, across all of the sources, in chronological order.
+// Simple, straight forward, but inefficient solution
+// Used to test the efficiency of the MinHeap solution
+const inefficientSolution = (logSources, printer) => {
+  let mergedLogs = [];
+  for (let i = 0; i < logSources.length; i++) {
+    let logEntry = null;
+    while((logEntry = logSources[i].pop())) {
+      mergedLogs.push(logEntry);
+    }
+  }
+
+  const sortedLogs = mergedLogs.sort((a, b) => a.date - b.date);
+
+  for (let i = 0; i < sortedLogs.length; i++) {
+    printer.print(sortedLogs[i]);
+  }
+
+  printer.done();
+}
+
+// Solution using the MinHeap
+const efficientSolution = (logSources, printer) => {
+  const minHeap = new MinHeap();
+
+  // Insert all elements into the min-heap
+  for (let i = 0; i < logSources.length; i++) {
+    let logEntry = null;
+    while((logEntry = logSources[i].pop())) {
+      minHeap.insert(logEntry);
+    }
+  }
+  
+  // Extract elements from the heap to get them in sorted order
+  while (!minHeap.isEmpty()) {
+    printer.print(minHeap.extractMin());
+  }  
+  printer.done();
+}
 
 module.exports = (logSources, printer) => {
+  // inefficientSolution(logSources, printer);
+  efficientSolution(logSources, printer);
   return console.log("Sync sort complete.");
 };
